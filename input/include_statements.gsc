af0edef29a6e80bc4214eb0e520267bc91ae8132
Max van Asten <maxvasten@gmail.com>
2025-02-11 21:25:16 +0100

    Better logging, test file

---
+++ input/include_statements.gsc
@@ -0,0 +1,4 @@
+#include common_scripts\utility;
+#include maps\mp\gametypes_zm\_hud_util;
+#include maps\mp\zombies\_zm_utility;
+#include some_script;
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 const input_files = fs.readdirSync(`./input`);
+
 console.log(`[gsc-ast] Attempting to parse input files...`);
 input_files.forEach((input_file) => {
     const name = input_file.split(".gsc")[0];
@@ -8,10 +9,14 @@ input_files.forEach((input_file) => {
     const content = fs.readFileSync(input_path, { encoding: "utf8" });
     const output = [];
 
+    console.log(`\t${name}[Tokenizer]`)
     // Tokenize content
 
 
+    console.log(`\t${name}[Parser]`)
     // Parse tokens
     
+    console.log(`\t${name}[Main]:`)
+    console.log(`\t\tWriting output to file ${output_path}`);
     fs.writeFileSync(output_path, JSON.stringify(output));
 })
